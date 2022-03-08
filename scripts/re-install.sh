dc590d5da1ef55108932a2b84e565fe73dd50ee5
Chia-Hao Chang <cuc1057@psu.edu>
2022-03-07 21:29:03 -0500

    Add re-install.sh
    Add re-install.sh for linking NVMe devices to libnvm kernel modules

---
+++ scripts/re-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash                                                                        
+set -e                                                                   
+                                                                                
+if [[ -d "/mount" ]]                                                              
+then
+  if [[ ! -z $(lsblk -l | grep "/mount") ]]; then
+    ret=$(umount /mount)
+  fi
+fi
+
+echo -n "0000:05:00.0" | sudo tee /sys/bus/pci/drivers/nvme/unbind
+echo -n "0000:05:00.0" | sudo tee /sys/bus/pci/drivers/libnvm/bind
+
+
+NVM_IDENTIFY_BIN=$(find ~/p2p-research -name "nvm-identify")
+if [[ ! -z "${NVM_IDENTIFY_BIN}" ]]
+then  
+  ${NVM_IDENTIFY_BIN} --ctrl=/dev/libnvm0
+fi
