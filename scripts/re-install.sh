2875181da34f7ce31e8488f726579f89bb470630
J <J>
2022-03-23 00:43:51 -0400

    Update scripts

--- scripts/re-install.sh
+++ scripts/re-install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash                                                                        
 set -e                                                                   
-                                                                                
-if [[ -d "/mount" ]]                                                              
+ 
+TARGET_DIR="/export/home2"
+CSL_HOME="/home/csl/cuc1057"
+
+if [[ -d "${TARGET_DIR}" ]]                                                              
 then
-  if [[ ! -z $(lsblk -l | grep "/mount") ]]; then
-    ret=$(umount /mount)
+  if [[ ! -z $(lsblk -l | grep "${TARGET_DIR}") ]]; then
+    ret=$(umount "${TARGET_DIR}")
   fi
 fi
 
-echo -n "0000:05:00.0" | sudo tee /sys/bus/pci/drivers/nvme/unbind
-echo -n "0000:05:00.0" | sudo tee /sys/bus/pci/drivers/libnvm/bind
+echo -n "0000:64:00.0" | sudo tee /sys/bus/pci/drivers/nvme/unbind
+echo -n "0000:64:00.0" | sudo tee /sys/bus/pci/drivers/libnvm/bind
 
 
-NVM_IDENTIFY_BIN=$(find ~/p2p-research -name "nvm-identify")
+NVM_IDENTIFY_BIN=$(find ${TARGET_DIR}/p2p-research -name "nvm-identify")
 if [[ ! -z "${NVM_IDENTIFY_BIN}" ]]
 then  
   ${NVM_IDENTIFY_BIN} --ctrl=/dev/libnvm0
